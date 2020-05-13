0881c41b5e35aee44fa4b26c0d18f7ef27db4171
Wyatt Johnson <me@wyattjoh.ca>
2020-05-13 14:43:35 -0600

    added web

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getGreetings() {
+    return this.appService.getGreetings();
+  }
+
+  @Get('/greet')
+  getHello(@Query('name') name?: string): string {
+    return this.appService.getHello(name);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,32 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private visits = new Map<string, number>();
+
+  getGreetings() {
+    const greetings = [];
+    for (const [name, count] of this.visits.entries()) {
+      greetings.push({ name, count });
+    }
+    return greetings;
+  }
+
+  getHello(name = 'World'): string {
+    let count = this.visits.get(name) || 0;
+    count++;
+    this.visits.set(name, count);
+
+    let msg = `Hello ${name}!`;
+
+    switch (count) {
+      case 1:
+        msg += ' This is your first time visiting!';
+        break;
+      default:
+        msg += ` We've seen you ${count} times!`;
+        break;
+    }
+
+    return msg;
   }
 }
